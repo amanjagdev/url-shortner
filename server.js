3a40b71a34450ca8cb6e93b644868ea6f047ee9f
amanjagdev <amankumarjagdev@gmail.com>
2020-06-01 17:12:29 +0530

    Fixed Deprecated Mongofb Warning

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
+      useUnifiedTopology: true
     });
     console.log("MONGODB Connected");
   } catch (err) {
@@ -18,7 +19,7 @@ connectDB();
 
 //Setup Server
 const app = express();
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5000;
 
 //Importing APIs
 const urlAPI = require("./api/url");
