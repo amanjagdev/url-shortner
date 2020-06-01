499218c4328dd07a654f4dc279e3784cd68ab243
amanjagdev <amankumarjagdev@gmail.com>
2020-06-01 16:00:58 +0530

    patch

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require("express");
 const app = express();
 const port = 5000 || process.env.PORT;
 
+app.use(express.json({extended: false}));
 
 app.get('/', (req,res) => {
     res.send("hi");
