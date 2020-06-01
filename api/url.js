a1e0aa7dfd81539c12a2720114c16bbb0e1fd288
amanjagdev <amankumarjagdev@gmail.com>
2020-06-01 16:22:22 +0530

    made shorten api

---
+++ api/url.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const router = express.Router();
+const validUrl = require("valid-url");
+const shortid = require("shortid");
+
+//Importing models
+const URL = require("../models/url");
+
+/*
+    @route:     POST /api/shorten
+    @desc:     To shorten the url
+*/
+router.post("/shorten", async (req, res) => {
+  const { longUrl } = req.body;
+  const baseUrl = process.env.BASE_URL;
+
+  // Check base url
+  if (!validUrl.isUri(baseUrl)) {
+    return res.status(401).json("Invalid base url");
+  }
+
+  // Create url code
+  const urlCode = shortid.generate();
+
+  // Check long url
+  if (validUrl.isUri(longUrl)) {
+    try {
+      let url = await URL.findOne({ longUrl });
+
+      if (url) {
+        res.json(url);
+      } else {
+        const shortUrl = `${baseUrl}/${urlCode}`;
+
+        url = new URL({
+          longUrl,
+          shortUrl,
+          urlCode,
+          date: new Date(),
+        });
+
+        await url.save();
+
+        res.json(url);
+      }
+    } catch (err) {
+      console.error(err);
+      res.status(500).json("Server error");
+    }
+  } else {
+    res.status(401).json("Invalid long url");
+  }
+});
+
+module.exports = router;
