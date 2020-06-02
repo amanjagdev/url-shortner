e6bc170524a57a658302f5351da3745aa46f9e32
amanjagdev <amankumarjagdev@gmail.com>
2020-06-02 13:30:26 +0530

    Addeed React Client

---
+++ client/build/precache-manifest.ea1226d69e406f842097b2d530b4cae4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a01571368878075b897d15359de1326e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0ed1df1d2c34d799535",
+    "url": "/static/js/2.b4e2a3f1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b4e2a3f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f14c4fdc99738fe0e00b",
+    "url": "/static/js/main.474e513c.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
